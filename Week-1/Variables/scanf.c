9b93f3c50ab22c9ada5bd80a5b6c422205257645
jstormatic <justinzhang202@gmail.com>
2023-12-16 21:58:32 +1100

    Uploading Progress

---
+++ Week-1/Variables/scanf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+    double avg_height;
+    printf("Enter your average height: ");
+    scanf("%lf", &avg_height);
+    printf("The average height is %lf\n", avg_height);
+    return 0;
+}
---
+++ Week-1/Variables/variables.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) {
+    int num_bees = 14;
+    double pi = 3.14159;
+    char initial = 'j';
+    printf("%c has %d bees\n", initial, num_bees);
+}
+
