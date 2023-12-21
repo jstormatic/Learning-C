0f19531c9ea6fa847874a6776767d49bf8c129d4
jstormatic <justinzhang202@gmail.com>
2023-12-21 17:28:46 +1100

    Uploading Progress

---
+++ Week-1/Variables/arrays-decay.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void double_int(int *int_to_double) {
+
+}
+
+int main(void) {
+
+    int data[5] = {1, 2, 3, 4, 5};
+
+    printf("%p\n", data);
+    printf("%d\n", *data);
+
+    printf("%p\n", &data[1]);
+    printf("%d\n", data[1]);
+
+    return 0;
+}
